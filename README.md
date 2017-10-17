e24cdcdf183b4b4eb4281a4de191b345dc80fa2b
daemondev <granlinux@gmail.com>
2017-10-16 20:01:21 -0500

    web-socket support added - (FAIL)

--- README.md
+++ README.md
@@ -1,3 +1,45 @@
-# pyCHAT
+# pyPANEL & pyCHAT - for pyBOT remote - management
 
-pyPanel and pyCHAT for pyBOT Management
+First
+=====
+
+Install [RethinkDB - https://www.rethinkdb.com/docs/install/ ](https://www.rethinkdb.com/docs/install/) and RUN: "rethinkdb --bind all"
+
+deps
+====
+
+- Flask==0.12.2
+- Flask-SocketIO==2.9.2
+- rethinkdb==2.3.0.post6
+- eventlet==0.21.0
+
+For initialize the RethinkDB database: Run before:
+
+python main.py --setup
+======================
+
+For try with ipython
+====================
+
+```python
+import rethinkdb as r
+from datetime import datetime
+
+conn = r.connect(host='localhost', port=28015, db='chat')
+
+n = 0
+new_chat = None
+def ins():
+    """Insert a new chat"""
+    global n
+    n = n + 1
+    data = {'name':'RethinkDB', 'message':'(%d .-) FROM SERVER' % n, 'created':str(datetime.now(r.make_timezone('00:00')))}
+    new_chat = r.table("chats").insert([ data ]).run(conn)
+
+def drop():
+    """Delete all chats (truncate)"""
+    r.db('chat').table('chats').delete().run(conn)
+
+drop()
+ins()
+```
--- main.py
+++ main.py
@@ -22,7 +22,6 @@ thread = None
 global main_thread
 main_thread = None
 
-# Load default config and override config from an environment variable
 app.config.update(dict(
     DEBUG=True,
     SECRET_KEY='secret!',
@@ -33,7 +32,6 @@ app.config.update(dict(
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 def init_db():
-    #conn = r.connect(app.config['DB_HOST'], app.config['DB_PORT'])
     conn = r.connect(host='localhost', port=28015)
     try:
         r.db_create(app.config['DB_NAME']).run(conn)
@@ -48,9 +46,9 @@ def init_db():
 @app.before_request
 def before_request():
     try:
-        g.db_conn = r.connect(host='localhost', port=28015,db='chat')
+        g.db_conn = r.connect(host='localhost', port=28015, db='chat')
     except RqlDriverError:
-        abort(503, "No database connection could be established.")
+        abort(503, "No database connection could be established. Try: python main.py --setup in CLI")
 
 @app.teardown_request
 def teardown_request(exception):
@@ -74,7 +72,6 @@ def list_shows():
     return render_template('chats.html', chats=chats)
 
 ######################################################################################
-
 @socketio.on("add_new_chat")
 def addNewChat(data):
     chatInput = data
@@ -85,97 +82,48 @@ def addNewChat(data):
 
     chatInput['created'] = str(chatInput['created'])
 
-    #chatInput = {'new_val':{'name':'ADMIN', 'message':'FROM SERVER', 'created':str(datetime.now(r.make_timezone('00:00')))}}
-
     socketio.emit('ws', chatInput)
-    #return make_response('success!', 201) ### Not needed
-
 ######################################################################################
 def watch_chats():
     #while 1:
     #socketio.sleep(1)
-    print('Watching db for new chats!')
-    conn = r.connect(host='localhost', port=28015,db='chat')
+    print('\n#################################\n###>>> Watching db for new chats!\n#################################\n\n')
+    conn = r.connect(host='localhost', port=28015, db='chat')
     feed = r.table("chats").changes().run(conn)
-    print('##########################\nin BUCLE WHILE\n#####################')
     for chat in feed:
         chat['new_val']['created'] = str(chat['new_val']['created'])
-        #global make_response
-
         socketio.emit('new_chat', chat)
-        #return make_response('success!', 201)
 ######################################################################################
 
 def main_thread_worker():
-    print('##########################\nin main thread')
+    print('##########################\nin main thread\n##########################')
     while 1:
         print('##########################\nin BUCLE WHILE\n#####################')
         socketio.sleep(1)
-        #the following emit works fine
 
-        conn = r.connect(host='localhost', port=28015,db='chat')
+        conn = r.connect(host='localhost', port=28015, db='chat')
         feed = r.table("chats").changes().run(conn)
-        print('DB Updated!!!')
         for chat in feed:
             chat['new_val']['created'] = str(chat['new_val']['created'])
             socketio.emit('new_chat', chat)
-            #return make_response('success!', 201)
 
 if __name__ == "__main__":
-    #https://github.com/miguelgrinberg/python-socketio/issues/16
-    #https://github.com/miguelgrinberg/Flask-SocketIO/issues/462
-    #https://flask-socketio.readthedocs.io/en/latest/#flask_socketio.SocketIO.emit
-    #https://github.com/miguelgrinberg/Flask-SocketIO/issues/117
-    # Set up rethinkdb changefeeds before starting server
-    #https://www.rethinkdb.com/docs/cookbook/javascript/
-    #https://stackoverflow.com/questions/30715780/reql-date-and-time-queries
-    #https://github.com/rethinkdb/rethinkdb/issues/3740
-    #https://github.com/rethinkdb/rethinkdb/issues/6285
-    #https://github.com/rethinkdb/rethinkdb/issues/997
-    parser = argparse.ArgumentParser(description='Run the Flask todo app')
+    parser = argparse.ArgumentParser(description='pyCHAT')
     parser.add_argument('--setup', dest='run_setup', action='store_true')
 
     args = parser.parse_args()
     if args.run_setup:
         init_db()
     else:
-        """
+        #"""
         if thread is None:
             thread = Thread(target=watch_chats)
             thread.setDaemon(True)
             thread.start() #"""
 
-        #"""
-        #if main_thread is None:
-        #main_thread = socketio.start_background_task(target=watch_chats)
-            #main_thread = socketio.start_background_task(target=main_thread_worker) #"""
+        """
+        if main_thread is None:
+            main_thread = socketio.start_background_task(target=watch_chats)
+            main_thread = socketio.start_background_task(target=main_thread_worker) #"""
 
         socketio.run(app, host='0.0.0.0', port=8000, debug=True)
-
-"""
-import rethinkdb as r
-from datetime import datetime
-
-conn = r.connect(host='localhost', port=28015,db='chat')
-r.db('chat').table('chats').delete().run(conn)
-
-n = 0
-new_chat = None
-def ins():
-    global n
-    n = n + 1
-    data = {'name':'ADMIN', 'message':'m(%d) - FROM SERVER' % n++, 'created':str(datetime.now(r.make_timezone('00:00')))}
-    new_chat = r.table("chats").insert([ data ]).run(conn)
-
-def el():
-    r.db('chat').table('chats').delete().run(conn)
-"""
-
-"""
-def listen():
-    while True:
-        watch_chats()
-        eventlet.sleep(5)
-
-eventlet.spawn(watch_chats) ###"""
-#eventlet.spawn(listen) ###"""
