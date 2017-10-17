8d68eb2c633e69ceddf3423fe3b88c7aff472ecb
daemondev <granlinux@gmail.com>
2017-10-16 19:14:08 -0500

    master - without eventlet (RUN)

--- README.md
+++ README.md
@@ -1,9 +1,32 @@
-# flask-rethinkdb-chat
-Of all the example projects on the rethinkdb site (https://www.rethinkdb.com/docs/examples/) the only one that showed a good use case of changefeeds was the CatThink project (https://www.rethinkdb.com/blog/cats-of-instagram/) which was written in JavaScript. There are some good example python projects but no one is using a changefeed in the python projects. I wanted to see if I could make a nice chat app with python socket-io and of course rethinkdb. This is the result.
+# pyPANEL & pyCHAT - for pyBOT remote - management
 
-The project has been set up to work with docker/docker-compose so in order to run the project just run `docker-compose build` then `docker-compose up`. You will notice an error from the web container. Just open a shell in the container and open up the python terminal from there and run: `from main import init_db` and then `init_db()`. Restart the containers and then you should be good to go!
+For initialize the RethinkDB database: Run before:
+
+python main.py --setup
+======================
 
 
-Demo:
 
-![](https://cloud.githubusercontent.com/assets/2521298/12210674/f3fcfde2-b618-11e5-99f2-57c3fa857f3b.gif)
+For try with ipython
+====================
+
+import rethinkdb as r
+from datetime import datetime
+
+conn = r.connect(host='localhost', port=28015, db='chat')
+
+n = 0
+new_chat = None
+def ins():
+    """Insert a new chat"""
+    global n
+    n = n + 1
+    data = {'name':'RethinkDB', 'message':'(%d .-) FROM SERVER' % n, 'created':str(datetime.now(r.make_timezone('00:00')))}
+    new_chat = r.table("chats").insert([ data ]).run(conn)
+
+def drop():
+    """Delete all chats (truncate)"""
+    r.db('chat').table('chats').delete().run(conn)
+
+drop()
+ins()
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from flask import Flask, g, render_template, make_response, request, redirect, url_for, jsonify
+from flask import Flask, g, render_template, make_response, request, redirect, url_for, jsonify, abort
 from flask_socketio import SocketIO, send, emit
 from threading import Thread
 import rethinkdb as r
-from rethinkdb import RqlRuntimeError
+from rethinkdb import RqlRuntimeError, RqlDriverError
 import json
 from datetime import datetime
 import argparse
@@ -12,19 +12,16 @@ socketio = SocketIO(app)
 global thread
 thread = None
 
-# Load default config and override config from an environment variable
 app.config.update(dict(
     DEBUG=True,
-    SECRET_KEY='secret!',
+    SECRET_KEY='apeponkopy!',
     DB_HOST='db',
     DB_PORT=28015,
     DB_NAME='chat'
 ))
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
-
 def init_db():
-    #conn = r.connect(app.config['DB_HOST'], app.config['DB_PORT'])
     conn = r.connect(host='localhost', port=28015)
     try:
         r.db_create(app.config['DB_NAME']).run(conn)
@@ -39,9 +36,9 @@ def init_db():
 @app.before_request
 def before_request():
     try:
-        g.db_conn = r.connect(host='localhost', port=28015,db='chat')
+        g.db_conn = r.connect(host='localhost', port=28015, db='chat')
     except RqlDriverError:
-        abort(503, "No database connection could be established.")
+        abort(503, "No database connection could be established. Try: python main.py --setup in CLI")
 
 @app.teardown_request
 def teardown_request(exception):
@@ -65,26 +62,19 @@ def list_shows():
     return render_template('chats.html', chats=chats)
 
 def watch_chats():
-    print('Watching db for new chats!')
-    conn = r.connect(host='localhost', port=28015,db='chat')
+    print('\n#################################\n###>>> Watching db for new chats!\n#################################\n\n')
+    conn = r.connect(host='localhost', port=28015, db='chat')
     feed = r.table("chats").changes().run(conn)
-    print('DB Updated!!!')
     for chat in feed:
         chat['new_val']['created'] = str(chat['new_val']['created'])
         socketio.emit('new_chat', chat)
 
-
 if __name__ == "__main__":
-    # Set up rethinkdb changefeeds before starting server
-    #https://www.rethinkdb.com/docs/cookbook/javascript/
-    #https://stackoverflow.com/questions/30715780/reql-date-and-time-queries
-    #https://github.com/rethinkdb/rethinkdb/issues/3740
-    #https://github.com/rethinkdb/rethinkdb/issues/6285
-    #https://github.com/rethinkdb/rethinkdb/issues/997
-    parser = argparse.ArgumentParser(description='Run the Flask todo app')
+    parser = argparse.ArgumentParser(description='pyCHAT')
     parser.add_argument('--setup', dest='run_setup', action='store_true')
 
     args = parser.parse_args()
+
     if args.run_setup:
         init_db()
     else:
